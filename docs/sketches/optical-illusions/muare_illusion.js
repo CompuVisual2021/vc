5ed80b51c9eacd9300586c8342a93de5699f559f
ferneysandoval <luisferneys@yahoo.com>
2021-04-02 22:10:50 +0000

    Deploying to gh-pages from  @ 4292d22ad19ce0da904ceca62e3c309580b300fb 🚀

---
+++ docs/sketches/optical-illusions/muare_illusion.js
@@ -0,0 +1,23 @@
+let x = 0;
+function setup() {
+    createCanvas(720, 500);
+}
+
+function draw() {
+    background(220);
+    for(let i=0; i<1000 ; i+=10) {
+        noFill();
+        stroke ('purple');
+        strokeWeight(3);
+        ellipse(360,250,i,i);
+
+        stroke('red');
+        strokeWeight(3);
+        ellipse(x, 250, i-500,i-500);
+    }
+    if(x > width){
+        x = 0;
+    } else {
+        x = x+3;
+    }
+}
